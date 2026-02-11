a79b69108413b1561844cec8412ca7a6b6400b19
Murali Shankar <mshankar@slac.stanford.edu>
2026-02-11 13:08:27 -0800

    RFC header in access logs

--- src/runscripts/run.sh
+++ src/runscripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PYTHONPATH="modules/flask_authnz:${PYTHONPATH}"
-export ACCESS_LOG_FORMAT='%(h)s %(l)s %({REMOTE_USER}i)s %(t)s "%(r)s" "%(q)s" %(s)s %(b)s %(D)s'
+export ACCESS_LOG_FORMAT='%(h)s %(l)s %({REMOTE-USER}i)s %(t)s "%(r)s" "%(q)s" %(s)s %(b)s %(D)s'
 export LOG_LEVEL=${LOG_LEVEL:-"INFO"}
 
 [ -z "$SERVER_IP_PORT" ] && export SERVER_IP_PORT="0.0.0.0:5000"
